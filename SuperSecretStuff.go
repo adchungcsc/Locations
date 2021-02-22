0b0efa67a12484eb4ca8579c8831a610a16e174d
adchungcsc <45158195+adchungcsc@users.noreply.github.com>
2021-02-22 14:50:11 -0500

    Create SuperSecretStuff.go

---
+++ SuperSecretStuff.go
@@ -0,0 +1,7 @@
+// please don't mine for bitcoin using this :(
+// ACCESS_KEY=AKIAIOSFOTESTEXAMPLE
+// SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRTESTEXAMPLEKEY
+
+username := "joe"
+
+connString = "SELECT * FROM LOGINS WHERE USERNAME = " + username
